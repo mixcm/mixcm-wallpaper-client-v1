e241acccd7cf0ca7fe7f62c16cc9965115d94703
折影轻梦 <776194970@qq.com>
2018-10-03 13:38:55 +0800

    更新了页面加载，搜索，分类

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,45 +1,125 @@
 <template>
-  <waterfall :line-gap="400" :min-line-gap="200" :max-line-gap="400" :watch="items" ref="waterfall">
-    {{ title }}
-    <!-- each component is wrapped by a waterfall slot -->
-    <waterfall-slot v-for="(item, index) in items" :width="item.width" :height="item.height" :order="index" :key="item.id">
-      <div class="mixcm-item">
-        <router-link tag="a" :to="'/detail/'+item.aid"><img v-lazy="item.urls.small"></router-link>
-        <div class="class"> {{ item.color }}</div>
-      </div>
-    </waterfall-slot>
-  </waterfall>
+  <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="1000"
+    infinite-scroll-throttle-delay="0">
+    {{ slug }} {{ page }}
+    <waterfall :line-gap="400" :min-line-gap="200" :max-line-gap="400" :watch="items" ref="waterfall">
+      <!-- each component is wrapped by a waterfall slot -->
+      <waterfall-slot v-for="(item, index) in items" :key="item.id" :width="item.width" :height="item.height" :order="index">
+        <div class="mixcm-item">
+          <router-link tag="a" :to="'/detail/'+item.aid"><img v-lazy="item.urls.small"></router-link>
+          <div class="class"> {{ item.color }}</div>
+        </div>
+      </waterfall-slot>
+    </waterfall>
+    <div v-show="loading" v-loading="loading" style="height: 120px;"></div>
+  </div>
 </template>
 
 <script>
   import Waterfall from 'vue-waterfall/lib/waterfall'
   import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
+  import infiniteScroll from 'vue-infinite-scroll'
 
   export default {
     data() {
       return {
-        items: String
+        busy: false,
+        loading: false,
+        page: this.$route.query.page,
+        slug: this.$route.query.slug,
+        keyword: this.$route.query.keyword,
+        items: String,
       }
     },
-    props: {
-      page: Number,
+    directives: {
+      infiniteScroll
+    },
+    methods: {
+      delUrlParam: function (url, ref) {
+        if (url.indexOf(ref) == -1) {
+          return url;
+        }
+        var arr_url = url.split('?');
+        var base = arr_url[0];
+        var arr_param = arr_url[1].split('&');
+        var index = -1;
+        var i;
+        for (i = 0; i < arr_param.length; i++) {
+          var paired = arr_param[i].split('=');
+          if (paired[0] == ref) {
+            index = i;
+            break;
+          }
+        }
+
+        if (index == -1) {
+          return url;
+        } else {
+          arr_param.splice(index, 1);
+          return base + "?" + arr_param.join('&');
+        }
+      },
+      loadInit: function () {
+        if (this.page == null) {
+          this.page = 1;
+        }
+        this.axios.get(this.GLOBAL.API+'/wallpaper', {
+            params: {
+              page: this.page,
+              slug: this.slug,
+              keywrod: this.keyword,
+            }
+          })
+          .then((res) => {
+            this.items = res.data;
+          })
+          .catch(function (error) {
+            console.log(error)
+          });
+      },
+      loadMore: function () {
+        this.busy = true;
+        this.loading = true;
+        this.page++;
+        this.axios.get(this.GLOBAL.API+'/wallpaper', {
+            params: {
+              page: this.page,
+              slug: this.slug,
+              keywrod: this.keyword,
+            }
+          })
+          .then((res) => {
+            if (res.data.length > 0) {
+              this.busy = false;
+              if(this.keyword!=null){
+                var url = this.delUrlParam(window.location.href, 'page');
+              }else{
+                var url = this.delUrlParam(window.location.href+'?home=yes', 'page');
+              }
+              history.pushState({}, "", url + "&page=" + this.page)
+              this.items = this.items.concat(res.data);
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        this.loading = false;
+      }
     },
     components: {
       Waterfall,
       WaterfallSlot
     },
     created: function () {
-      this.axios.get('http://localhost:1002/api/index.php/wallpaper', {
-          params: {
-            page: this.page,
-          }
-        })
-        .then((res) => {
-          this.items = res.data;
-        })
-        .catch(function (error) {
-          console.log(error)
-        });
+      this.loadInit();
+    },
+    watch: {
+      $route(to, from) {
+        this.slug = this.$route.params.slug;
+        this.page = this.$route.params.page;
+        this.keyword = this.$route.query.keyword;
+        this.loadInit();
+      }
     }
   }
 
@@ -47,23 +127,20 @@
 
 <style>
   #mixcm-content .mixcm-item {
-    background-color: #f3f3f3;
+    background-color: #fbfbfb;
     height: calc(100% - 20px);
     width: calc(100% - 20px);
     margin: 10px;
     border-radius: 3px;
     overflow: hidden;
-    animation: loading .7s infinite linear alternate;
+    background-position: center;
+    background-size: 200px;
+    background-repeat: no-repeat;
+    background-image: url('../assets/loading.gif');
   }
 
-  @keyframes loading {
-    0% {
-      background-color: #f3f3f3;
-    }
-
-    to {
-      background-color: #eaeaea;
-    }
+  .vue-waterfall {
+    min-height: 1500px;
   }
 
   #mixcm-content .mixcm-item img {
@@ -82,7 +159,7 @@
     display: block;
     position: absolute;
     right: 0;
-    top: 20px;
+    top: 25px;
     z-index: 5;
     border-radius: 0 3px 3px 0;
   }
