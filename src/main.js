43ae1c2c4b977851368be6fde6b63d16b144ee53
折影轻梦 <776194970@qq.com>
2019-01-19 20:28:33 +0800

    bug fix

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import ElementUI from 'element-ui';
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import axios from 'axios'
@@ -12,8 +12,8 @@ import VueAxios from 'vue-axios'
 
 import VueLazyload from 'vue-lazyload'
 
-import global from '@/components/Global';
-Vue.prototype.GLOBAL = global;
+import global from '@/components/Global'
+Vue.prototype.GLOBAL = global
 
 Vue.use(VueLazyload)
 Vue.use(ElementUI)
